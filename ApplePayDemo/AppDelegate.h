fa3f50ce5835c6772583f3ad0248c0870e416a8a
LZMrRight <275068651@qq.com>
2016-03-21 16:34:01 +0800

    Initial Commit

---
+++ ApplePayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayDemo
+//
+//  Created by Mr.Right on 16/3/21.
+//  Copyright © 2016年 lizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
